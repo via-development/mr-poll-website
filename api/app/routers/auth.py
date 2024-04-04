64d1929b8357853790e16ea41f3ee3271497977d
Mr-Conos <conosv08@gmail.com>
2024-04-03 18:59:38 -0700

    Authentication module

--- api/app/routers/auth.py
+++ api/app/routers/auth.py
@@ -1,4 +1,18 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import JSONResponse
 
+from ..dependencies import discord
+
 router = APIRouter()
+
+@router.get('/auth/login')
+async def authentication():
+    return JSONResponse({"url":discord.oauth_login_url},200)
+
+@router.get("/auth/callback")
+async def callback(code:str):
+    try:
+        token, refresh_token = await discord.get_access_token(code)
+        return JSONResponse({"success": True, "token": token}, 200)
+    except:
+        return JSONResponse({"success": False},400)
